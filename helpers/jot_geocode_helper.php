c637e567993aefb4ccb2ba57041b482d8014f995
Tom Krush <t.krush@gmail.com>
2012-06-06 13:47:21 -0500

    Updated geocode to use google api

--- helpers/jot_geocode_helper.php
+++ helpers/jot_geocode_helper.php
@@ -4,32 +4,30 @@ if ( ! function_exists('geocode') )
 {
 	function geocode($address)
 	{	
-		$url = "http://where.yahooapis.com/geocode?flags=P&q=".urlencode($address);
-		
-			
+		$url = "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=".urlencode($address);
+
 		$ch = curl_init($url);
 	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-	    $data = unserialize(curl_exec($ch));
-
+	    $data = curl_exec($ch);
+		
 	    curl_close($ch);
 		
 		if ( $data == false ) return false;
-				
-		$data = value_for_key('ResultSet.Result', $data);
-							
-		if ( is_array($data) && count($data) && $data = array_shift($data) )
+
+		$data = json_decode($data, true);
+
+		$data = value_for_key('results.0.geometry.location', $data);
+		
+		$latitude = value_for_key('lat', $data);
+		$longitude = value_for_key('lng', $data);
+		
+		if ( $latitude && $longitude)
 		{
-			$latitude = value_for_key('latitude', $data);
-			$longitude = value_for_key('longitude', $data);
-			
-			if ( $latitude && $longitude)
-			{
-				return array(
-					'latitude' => $latitude,
-					'longitude' => $longitude
-				);
-			}
+			return array(
+				'latitude' => $latitude,
+				'longitude' => $longitude
+			);
 		}
 		
 		return false;
